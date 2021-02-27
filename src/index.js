66a7fa4bc917418b15a7ee20d9765f07fe04c1a6
sneha <snehaskannan@gmail.com>
2021-02-28 01:13:54 +0530

    templateadded

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes';
 // import * as serviceWorker from './serviceWorker';
-
+import './assets/scss/style.scss';
 ReactDOM.render(<Routes/>,document.getElementById('root'));
